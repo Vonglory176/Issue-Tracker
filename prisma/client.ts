696dadf6e9293b387286223ef5ee5bbeb7203315
Vonglory176 <SkylerGConley@gmail.com>
2025-01-07 16:22:08 -0800

    Initialized Prisma & API's

---
+++ prisma/client.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client'
+
+const prismaClientSingleton = () => {
+  return new PrismaClient()
+}
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined
+}
+
+export const prisma = globalForPrisma.prisma ?? prismaClientSingleton()
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+
+// https://youtu.be/J9sfR6HN6BY?t=2463 || 41:03
+
+
+
+
+
+
+// // lib/prisma.ts
+// import { PrismaClient } from "@prisma/client";
+
+// const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+// export const prisma = globalForPrisma.prisma || new PrismaClient();
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
